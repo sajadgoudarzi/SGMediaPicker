57ed4fd049db6fcc467fb2948f2d821bad7115eb
Aban <aban@Macs-iMac.local>
2018-03-06 08:17:47 +0330

    first commit

---
+++ TZImagePickerController/SGCustomBottomBarOnPreview.m
@@ -0,0 +1,29 @@
+//
+//  SGCustomBottomBarOnPreview.m
+//  TZImagePickerController
+//
+//  Created by Aban on 4/17/17.
+//  Copyright © 2017 谭真. All rights reserved.
+//
+
+#import "SGCustomBottomBarOnPreview.h"
+
+
+@implementation SGCustomBottomBarOnPreview
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    
+    return self;
+}
+
+
+- (void)createBottomBar
+{
+    
+}
+
+@end
